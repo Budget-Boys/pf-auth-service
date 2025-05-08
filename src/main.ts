edfaca32fcd452586315503a2dbebdfe0f020bef
Willian <slawtow@gmail.com>
2025-05-08 19:33:22 -0300

    feat: basic settings

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('BB-Authentication')
+    .setDescription('Module for user authentication')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, configSwagger, {
+    operationIdFactory: (_controllerKey: string, methodKey: string) => methodKey,
+  });
+
+  SwaggerModule.setup('swagger', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
